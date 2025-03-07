afdaf4e9942ce2cf1715df0db09823096ca58d83
baskan07 <ardabaskan07@yaani.com>
2025-03-07 12:51:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def get_data():
     return jsonify([{"latitude": row[0], "longitude": row[1], "temperature": row[2], "humidity": row[3], "timestamp": row[4]} for row in data])
 
 @app.route('/data2', methods=['GET'])
-def get_data():
+def get_data2():
     cursor.execute("SELECT latitude, longitude, timestamp, servername FROM sunucu_okuma")
     data = cursor.fetchall()
     return jsonify([{"latitude": row[0], "longitude": row[1], "timestamp": row[2], "servername": row[3]} for row in data])
